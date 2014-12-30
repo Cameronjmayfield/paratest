f6c215cde4baa35481cd16b558c780b56ba9d0b7
CameronJMayfield <cmayfield@dandb.com>
2014-12-30 14:46:34 -0800

    Prefers class names to match file names. Falls back on any class in the file.

--- test/unit/ParaTest/Parser/ParserTest.php
+++ test/unit/ParaTest/Parser/ParserTest.php
@@ -9,4 +9,19 @@ class ParserTest extends \TestBase
     {
         $parser = new Parser('/path/to/nowhere');
     }
+
+    public function testPrefersClassByFileName()
+    {
+        $filename = FIXTURES . DS . 'special-classes' . DS . 'ParserTestClass.php';
+        $parser = new Parser($filename);
+        $this->assertEquals("ParserTestClass", $parser->getClass()->getName());
+    }
+
+    public function testClassFallsBackOnExisting()
+    {
+        $filename = FIXTURES . DS . 'special-classes' . DS . 'NameDoesNotMatch.php';
+        $parser = new Parser($filename);
+        $this->assertEquals("ParserTestClassFallsBack", $parser->getClass()->getName());
+    }
+
 }
